2b6e5b81f8ca25189d85ac01efb0155156a28acc
Marius <marius.sergent@gmail.com>
2025-04-15 11:29:10 +0200

    add logout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,18 @@
+import { useAuth } from '../contexts/AuthContext'
+
 function Home () {
+  const { logout } = useAuth()
+
   return (
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+      <button
+        onClick={logout}
+        className='bg-red-400 hover:bg-green-200 px-4 py-2 rounded-lg cursor-pointer shadow-md transition-all duration-200'
+      >
+        Se déconnecter
+      </button>
+    </>
   )
 }
 
