26f22cb043e7835289322f993f1a00acff44b909
Marius <marius.sergent@gmail.com>
2025-04-15 14:19:41 +0200

    add avatar and button

--- frontend/src/components/sections/ProfileSection.jsx
+++ frontend/src/components/sections/ProfileSection.jsx
@@ -1,7 +1,14 @@
+import Button from '../button'
+
 function ProfileSection () {
   return (
     <section className='flex flex-col w-full h-full mx-auto justify-center items-center gap-4 bg-white shadow-md rounded-lg p-4 max-w-md'>
       <h2 className='text-2xl font-semibold'>Mon Profil</h2>
+      <Button
+        variant='info'
+      >
+        Générer mon personnage
+      </Button>
     </section>
   )
 }
