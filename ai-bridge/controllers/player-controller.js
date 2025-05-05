ea051aedd3617376fcef2e9c9031fdf122161528
Marius <marius.sergent@gmail.com>
2025-05-05 15:21:50 +0200

    fix param sending to ai

--- ai-bridge/controllers/player-controller.js
+++ ai-bridge/controllers/player-controller.js
@@ -1,8 +1,15 @@
 const { generateText } = require('../adapters/gemini-adapter')
 
 const generatePlayer = async (data) => {
+  console.log(data)
   const aiResponse = await generateText(
-    `Génère un personnage de jeu RPG en fonction des paramètres suivants ${data}`
+    `
+    Génère un personnage de jeu RPG en fonction des paramètres suivants :
+     Nom: ${data.name}
+     Classe: ${data.class}
+     Univers: Dark fantasy medieval
+    Soit drôle
+    `
   )
   console.log(aiResponse)
 }
