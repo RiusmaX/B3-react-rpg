482554f7807b5b23f0d70b4a4585059d09420fa4
Marius <marius.sergent@gmail.com>
2025-05-05 15:26:26 +0200

    change response format

--- ai-bridge/controllers/player-controller.js
+++ ai-bridge/controllers/player-controller.js
@@ -8,7 +8,8 @@ const generatePlayer = async (data) => {
      Nom: ${data.name}
      Classe: ${data.class}
      Univers: Dark fantasy medieval
-    Soit drôle
+    Soit drôle.
+    Répond en format JSON avec les champs "name", "description"
     `
   )
   console.log(aiResponse)
