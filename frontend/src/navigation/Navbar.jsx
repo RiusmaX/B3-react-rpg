8a889d8ae8ad9647bd8eb6a9e1ea4b3d6c6ffc0d
Marius <marius.sergent@gmail.com>
2025-05-20 09:57:45 +0200

    move assets

--- frontend/src/navigation/Navbar.jsx
+++ frontend/src/navigation/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar () {
     <nav className='flex flex-row w-full justify-center items-center px-6 max-w-[1440px]'>
       <div className='flex flex-row w-full justify-between items-center'>
         <a className='navbar-brand opacity-100 hover:opacity-60 transition-all duration-300' href='/'>
-          <img src='/src/assets/logo.webp' alt='logo' className='h-20 w-20' />
+          <img src='/assets/logo.webp' alt='logo' className='h-20 w-20' />
         </a>
         <Link
           to='/profile'
